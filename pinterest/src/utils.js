0625b0d3b2daed6c1a7628a7b3962e9cbe618bd2
Fatemeh <uni.khosravi@gmail.com>
2025-03-29 17:52:09 +0330

    feat: add smart retries

--- pinterest/src/utils.js
+++ pinterest/src/utils.js
@@ -15,11 +15,25 @@ export const getShortestColumn = (columns) => {
 };
 
 export const fetchPins = async (page = 1, limit = 10) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      const start = (page - 1) * limit;
-      const end = start + limit;
-      resolve(PIN_DATA.items.slice(start, end));
-    }, 1000);
-  });
+  const MAX_RETRIES = 3;
+  const INITIAL_DELAY = 1000;
+  const fetchWithRetry = async () => {
+    await new Promise((res) => setTimeout(res, INITIAL_DELAY));
+    const start = (page - 1) * limit;
+    const end = start + limit;
+    const pins = PIN_DATA.items.slice(start, end);
+    if (pins?.length === 0) {
+      throw new Error("No More Pins Available");
+    }
+    return pins;
+  };
+  for (let attemp = 0; attemp < MAX_RETRIES; attemp++) {
+    try {
+      return await fetchWithRetry();
+    } catch (error) {
+      if (attemp === MAX_RETRIES - 1) {
+        throw error;
+      }
+    }
+  }
 };
